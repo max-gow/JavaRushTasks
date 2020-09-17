f40b79d7c837375d3b107d90f3ce6eae525337b2
max-gow <64403832+max-gow@users.noreply.github.com>
2020-09-17 09:07:17 +0300

    temp

--- 1.JavaSyntax/src/com/javarush/task/task04/task0442/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0442/Solution.java
@@ -7,6 +7,7 @@ package com.javarush.task.task04.task0442;
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+        //what
 
     }
 }
